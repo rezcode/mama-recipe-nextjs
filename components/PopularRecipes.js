13610a7c6d918d3e586f215400b31ac406ff0335
rezcode <rezharians@gmail.com>
2022-08-06 01:33:35 +0800

    ADD get newRecipe, newPopular, add link to populare page

--- components/PopularRecipes.js
+++ components/PopularRecipes.js
@@ -4,44 +4,36 @@ import imgRecipe from "../public/images/example-img.jpg";
 import { AiFillStar } from "react-icons/ai";
 import homeStyles from "../styles/Home.module.css";
 
-const PopularRecipes = () => {
-  return (
-    <>
-      <div className="row mb-3">
-        <div className="col-3">
-          <Image src={imgRecipe} className={homeStyles.imagePopular} />
-        </div>
-        <div className="col-9">
-          <p className={homeStyles.title}>Orange La Pasta</p>
-          <p className={homeStyles.rating}>
-            <AiFillStar color="#FFB300" /> 4.6
-          </p>
-        </div>
-      </div>
-      <div className="row mb-3">
-        <div className="col-3">
-          <Image src={imgRecipe} className={homeStyles.imagePopular} />
-        </div>
-        <div className="col-9">
-          <p className={homeStyles.title}>Orange La Pasta</p>
-          <p className={homeStyles.rating}>
-            <AiFillStar color="#FFB300" /> 4.6
-          </p>
-        </div>
-      </div>
-      <div className="row mb-3">
-        <div className="col-3">
-          <Image src={imgRecipe} className={homeStyles.imagePopular} />
-        </div>
-        <div className="col-9">
-          <p className={homeStyles.title}>Orange La Pasta</p>
-          <p className={homeStyles.rating}>
-            <AiFillStar color="#FFB300" /> 4.6
-          </p>
-        </div>
-      </div>
-    </>
-  );
+const PopularRecipes = (props) => {
+  const renderPopularRecipe = () => {
+    const jsx = props?.data?.map((item, index) => {
+      return (
+        <>
+          <div key={index} className="row mb-3">
+            <div className={`col-3 ${homeStyles.imagePopular}`}>
+              <Image
+                src={`http://localhost:8000/${item.food_image.replace(
+                  "public/",
+                  ""
+                )}`}
+                height={64}
+                width={64}
+              />
+            </div>
+            <div className="col-9">
+              <p className={homeStyles.title}>{item.title}</p>
+              <p className={homeStyles.rating}>
+                <AiFillStar color="#FFB300" /> 4.6 <span>{item.category}</span>
+              </p>
+            </div>
+          </div>
+        </>
+      );
+    });
+    return jsx;
+  };
+
+  return <>{renderPopularRecipe()}</>;
 };
 
 export default PopularRecipes;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewRecipes from "../components/NewRecipes";
 import Footer from "../components/Footer";
 import PopularRecipes from "../components/PopularRecipes";
 import { BiSearch } from "react-icons/bi";
+import Link from "next/link";
+import homeStyle from "../styles/Home.module.css";
 
-const home = () => {
+const home = (props) => {
+  const [newRecipe] = useState(props?.newRecipes?.data);
+  const [popularRecipe] = useState(props?.popularRecipes?.data);
+
+  useEffect(() => {
+    newRecipe;
+    popularRecipe;
+  }, []);
+
   return (
     <>
       <div>
@@ -27,12 +37,19 @@ const home = () => {
                     <h5>New Recipes</h5>
                   </div>
                   <div className="row">
-                    <NewRecipes />
+                    <NewRecipes data={newRecipe} />
                   </div>
                   <div className="row mt-5 mb-3">
-                    <h5>Popular Recipes</h5>
+                    <div className="col-6">
+                      <h5>Popular Recipes</h5>
+                    </div>
+                    <div className={`col-5 text-end ${homeStyle.pointer}`}>
+                      <Link href="/popular-menu">
+                        <span>more info</span>
+                      </Link>
+                    </div>
                   </div>
-                  <PopularRecipes />
+                  <PopularRecipes data={popularRecipe} />
                 </div>
               </div>
             </div>
@@ -43,5 +60,21 @@ const home = () => {
     </>
   );
 };
+
+export async function getServerSideProps(context) {
+  const [newRecipesRes, popularRecipesRes] = await Promise.all([
+    fetch("http://localhost:8000/recipes/find/recent"),
+    fetch("http://localhost:8000/recipes/popular"),
+  ]);
+
+  const [newRecipes, popularRecipes] = await Promise.all([
+    newRecipesRes.json(),
+    popularRecipesRes.json(),
+  ]);
+
+  return {
+    props: { newRecipes, popularRecipes },
+  };
+}
 
 export default home;

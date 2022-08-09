53c42b5d4917f5b2a4665f4ff422c1f69d6994cc
rezcode <rezharians@gmail.com>
2022-08-09 12:31:22 +0800

    last push for pressentation

--- pages/profile/liked-recipe.js
+++ pages/profile/liked-recipe.js
@@ -1,44 +1,86 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import popularMenuStyle from "../../styles/popularMenu.module.css";
 import Image from "next/image";
-import imgRecipe from "../../public/images/example-img.jpg";
 import homeStyles from "../../styles/Home.module.css";
 import HeaderProfile from "../../components/headerProfile";
+import axios from "axios";
+import Link from "next/link";
+import { TbDatabaseOff } from "react-icons/tb";
 
 const LikedRecipe = () => {
+  const [myLikeRecipe, setMyLikeRecipe] = useState([]);
+  const [userDataStorage, setUserDataStorage] = useState({});
+
+  useEffect(() => {
+    setUserDataStorage(JSON.parse(localStorage?.getItem("userDataStorage")));
+    getMyLikeRecipe();
+  }, [userDataStorage?.id]);
+
+  const config = {
+    headers: {
+      Authorization: `Bearer ${userDataStorage?.token}`,
+    },
+  };
+
+  const getMyLikeRecipe = () => {
+    axios
+      .get(
+        `${process.env.NEXT_PUBLIC_URL_API}/recipes/my/like/${userDataStorage?.id}`,
+        config
+      )
+      .then((res) => {
+        console.log(res?.data?.data);
+        setMyLikeRecipe(res?.data?.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <>
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-md-4">
             <HeaderProfile header={"Liked Recipe"} />
-            <div className="row mt-3 align-items-center">
-              <div className="col-3" style={{ paddingRight: "0px" }}>
-                <Image src={imgRecipe} className={homeStyles.imagePopular} />
-              </div>
-              <div className={`col-9 ${popularMenuStyle.contentTitle}`}>
-                <p className={popularMenuStyle.title}>Margherita</p>
-                <p className={popularMenuStyle.ingredients}>In Veg Pizza</p>
-              </div>
-            </div>
-            <div className="row mt-3 align-items-center">
-              <div className="col-3" style={{ paddingRight: "0px" }}>
-                <Image src={imgRecipe} className={homeStyles.imagePopular} />
-              </div>
-              <div className={`col-9 ${popularMenuStyle.contentTitle}`}>
-                <p className={popularMenuStyle.title}>Margherita</p>
-                <p className={popularMenuStyle.ingredients}>In Veg Pizza</p>
-              </div>
-            </div>
-            <div className="row mt-3 align-items-center">
-              <div className="col-3" style={{ paddingRight: "0px" }}>
-                <Image src={imgRecipe} className={homeStyles.imagePopular} />
-              </div>
-              <div className={`col-9 ${popularMenuStyle.contentTitle}`}>
-                <p className={popularMenuStyle.title}>Margherita</p>
-                <p className={popularMenuStyle.ingredients}>In Veg Pizza</p>
-              </div>
-            </div>
+            {myLikeRecipe?.length === 0 ? (
+              <>
+                <div className="container">
+                  <div className="row mt-5">
+                    <TbDatabaseOff size={150} />
+                    <p className="text-center">No Recipe</p>
+                  </div>
+                </div>
+              </>
+            ) : (
+              myLikeRecipe?.map((item, index) => (
+                <Link href={`/recipe/${item.id_recipe}`}>
+                  <div
+                    className="row mt-3 align-items-center"
+                    key={index}
+                    style={{ cursor: "pointer" }}
+                  >
+                    <div className={`col-3 ${popularMenuStyle.imgList}`}>
+                      <Image
+                        src={`http://localhost:8000/${item.food_image?.replace(
+                          "public/",
+                          ""
+                        )}`}
+                        height={110}
+                        width={110}
+                        className={homeStyles.imagePopular}
+                      />
+                    </div>
+                    <div className={`col-9 ${popularMenuStyle.contentTitle}`}>
+                      <p className={popularMenuStyle.title}>{item.title}</p>
+                      <p className={popularMenuStyle.ingredients}>
+                        {item.ingredients?.split(" ").slice(0, 2).join(" ")}
+                      </p>
+                      <p className={popularMenuStyle.title}>{item.category}</p>
+                    </div>
+                  </div>
+                </Link>
+              ))
+            )}
           </div>
         </div>
       </div>
--- pages/profile/saved-recipe.js
+++ pages/profile/saved-recipe.js
@@ -1,44 +1,86 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import popularMenuStyle from "../../styles/popularMenu.module.css";
 import Image from "next/image";
-import imgRecipe from "../../public/images/example-img.jpg";
 import homeStyles from "../../styles/Home.module.css";
 import HeaderProfile from "../../components/headerProfile";
+import axios from "axios";
+import { TbDatabaseOff } from "react-icons/tb";
+import Link from "next/link";
 
 const SavedRecipe = () => {
+  const [myRecipeList, setMyRecipeList] = useState([]);
+  const [userDataStorage, setUserDataStorage] = useState({});
+
+  useEffect(() => {
+    setUserDataStorage(JSON.parse(localStorage?.getItem("userDataStorage")));
+    getMyRecipeList();
+  }, [userDataStorage?.id]);
+
+  const config = {
+    headers: {
+      Authorization: `Bearer ${userDataStorage?.token}`,
+    },
+  };
+
+  const getMyRecipeList = () => {
+    axios
+      .get(
+        `${process.env.NEXT_PUBLIC_URL_API}/recipes/my/save/${userDataStorage?.id}`,
+        config
+      )
+      .then((res) => {
+        setMyRecipeList(res?.data?.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-md-4">
-            <HeaderProfile header={"Saved Recipe"} />
-            <div className="row mt-3 align-items-center">
-              <div className="col-3" style={{ paddingRight: "0px" }}>
-                <Image src={imgRecipe} className={homeStyles.imagePopular} />
-              </div>
-              <div className={`col-9 ${popularMenuStyle.contentTitle}`}>
-                <p className={popularMenuStyle.title}>Margherita</p>
-                <p className={popularMenuStyle.ingredients}>In Veg Pizza</p>
-              </div>
-            </div>
-            <div className="row mt-3 align-items-center">
-              <div className="col-3" style={{ paddingRight: "0px" }}>
-                <Image src={imgRecipe} className={homeStyles.imagePopular} />
-              </div>
-              <div className={`col-9 ${popularMenuStyle.contentTitle}`}>
-                <p className={popularMenuStyle.title}>Margherita</p>
-                <p className={popularMenuStyle.ingredients}>In Veg Pizza</p>
-              </div>
-            </div>
-            <div className="row mt-3 align-items-center">
-              <div className="col-3" style={{ paddingRight: "0px" }}>
-                <Image src={imgRecipe} className={homeStyles.imagePopular} />
-              </div>
-              <div className={`col-9 ${popularMenuStyle.contentTitle}`}>
-                <p className={popularMenuStyle.title}>Margherita</p>
-                <p className={popularMenuStyle.ingredients}>In Veg Pizza</p>
-              </div>
-            </div>
+            <HeaderProfile header={"My Recipe"} />
+            {myRecipeList?.length === 0 ? (
+              <>
+                <div className="container">
+                  <div className="row mt-5">
+                    <TbDatabaseOff size={150} />
+                    <p className="text-center">No Recipe</p>
+                  </div>
+                </div>
+              </>
+            ) : (
+              myRecipeList?.map((item, index) => (
+                <Link href={`/recipe/${item.id_recipe}`}>
+                  <div
+                    className="row mt-3 align-items-center"
+                    key={index}
+                    style={{ cursor: "pointer" }}
+                  >
+                    <div className={`col-3 ${popularMenuStyle.imgList}`}>
+                      <Image
+                        src={`http://localhost:8000/${item.food_image?.replace(
+                          "public/",
+                          ""
+                        )}`}
+                        height={110}
+                        width={110}
+                        className={homeStyles.imagePopular}
+                      />
+                    </div>
+                    <div className={`col-9 ${popularMenuStyle.contentTitle}`}>
+                      <p className={popularMenuStyle.title}>{item.title}</p>
+                      <p className={popularMenuStyle.ingredients}>
+                        {item.ingredients?.split(" ").slice(0, 2).join(" ")}
+                      </p>
+                      <p className={popularMenuStyle.title}>{item.category}</p>
+                    </div>
+                  </div>
+                </Link>
+              ))
+            )}
           </div>
         </div>
       </div>
